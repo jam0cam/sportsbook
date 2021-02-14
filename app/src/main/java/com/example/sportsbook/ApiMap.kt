b84a4b0cd0688378aeda9771b405e442a0f37493
Jia Tse <jia.tse@zocdoc.com>
2021-02-14 11:08:54 -0500

    added cookies to each request for auth

--- app/src/main/java/com/example/sportsbook/ApiMap.kt
+++ app/src/main/java/com/example/sportsbook/ApiMap.kt
@@ -16,18 +16,19 @@ class ApiMap @Inject constructor(
     service: ApiService,
     private val parser: BetParser
 ) {
+    val cookie = "ruibj=live%20betting; ruibj=default; JSESSIONID=SBK5~F686116CA8A3D4A4387628AB2FA058FF; timeZoneId=US/Eastern; oddsformat=AMERICAN; defaultbetamount=0; autoAcceptChange=NO; showCashoutConf=YES; JSESSIONID=SBK5~F686116CA8A3D4A4387628AB2FA058FF; SBK_JSESSIONID=SBK5~F686116CA8A3D4A4387628AB2FA058FF; timeZoneId=US/Eastern; oddsformat=AMERICAN; defaultbetamount=0; autoAcceptChange=NO; showCashoutConf=YES; ruitimeoutCookie=false; _ga=GA1.2.1207703326.1561983414; CCA_JSESSIONID=B7B7702D28662AEAFFEFE9FA78C4ECAB; everlogged=true; has_js=1; CCAREFINERY=CCA2|XoTJg; visid_incap_2286874=G1u9mI5ZQM2k4KNYZl2PnMDOpV4AAAAAQUIPAAAAAABnJgekzseGGFzP+nyoR5xG; CCA_JSESSIONID=B7B7702D28662AEAFFEFE9FA78C4ECAB; everlogged=true; _gid=GA1.2.1966506178.1610737008; incap_ses_8217_2286874=6HxXTntAdyNGHuXdC6YIcrLrJ2AAAAAAyCxC7Tf6iW5e7ZhK/DNENQ==; CASTGC=TGT-1613230438902-d5aM7FCONBHMlNyH5gRF37ujIcrgxW; CCAREFINERY=CCA1|YCfxU"
     val map1 = mapOf(
-        NCAAF to service.getNcaaFootballLines().getData(NCAAF),
-        NFL to service.getNflLines().getData(NFL),
-        NCAAB to service.getNcaaBasketball().getData(NCAAB),
-        ESPORTS to service.getEsports().getData(ESPORTS),
-        TABLE_TENNIS to service.getTableTennis().getData(TABLE_TENNIS),
-        NBA_PRESEASON to service.getNbaPreseason().getData(NBA_PRESEASON),
-        NBA to service.getNba().getData(NBA),
-        UFC to service.getUfc().getData(UFC),
-        BOXING to service.getBoxing().getData(BOXING),
-        TENNIS to service.getTennis().getData(TENNIS),
-        SOCCER to service.getSoccer().getData(SOCCER),
+        NCAAF to service.getNcaaFootballLines(cookie).getData(NCAAF),
+        NFL to service.getNflLines(cookie).getData(NFL),
+        NCAAB to service.getNcaaBasketball(cookie).getData(NCAAB),
+        ESPORTS to service.getEsports(cookie).getData(ESPORTS),
+        TABLE_TENNIS to service.getTableTennis(cookie).getData(TABLE_TENNIS),
+        NBA_PRESEASON to service.getNbaPreseason(cookie).getData(NBA_PRESEASON),
+        NBA to service.getNba(cookie).getData(NBA),
+        UFC to service.getUfc(cookie).getData(UFC),
+        BOXING to service.getBoxing(cookie).getData(BOXING),
+        TENNIS to service.getTennis(cookie).getData(TENNIS),
+        SOCCER to service.getSoccer(cookie).getData(SOCCER),
     )
 
     /**
